d17a3281db9819065107c5f11048b309a8f03135
NokoReach932 <suonokoreach@gmail.com>
2025-05-25 16:23:47 +0700

    Update server.js

--- server.js
+++ server.js
@@ -98,10 +98,24 @@ app.delete("/articles/:id", (req, res) => {
 
 // --- Categories ---
 // Get all categories
-app.get("/categories", (req, res) => {
-  const categories = readJSON(categoriesFile);
-  res.json(categories);
+app.get("/articles", (req, res) => {
+  let articles = readJSON(articlesFile);
+  const { category, excludeId } = req.query;
+
+  if (category) {
+    articles = articles.filter(article => article.category === category);
+  }
+
+  if (excludeId) {
+    const excludeIdNum = parseInt(excludeId);
+    if (!isNaN(excludeIdNum)) {
+      articles = articles.filter(article => article.id !== excludeIdNum);
+    }
+  }
+
+  res.json(articles);
 });
+
 
 // Add a new category
 app.post("/categories", (req, res) => {
