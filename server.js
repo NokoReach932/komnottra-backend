8e13e782678f263372e5bd10315e8435e545c576
NokoReach932 <suonokoreach@gmail.com>
2025-05-28 18:29:34 +0700

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ const PORT = process.env.PORT || 5000;
 // === CORS Setup ===
 const allowedOrigins = [
   "https://www.komnottra.com",
-  "https://www.komnottra.com:10000"
   "https://komnottra.com",
   "http://localhost:10000"
 ];
